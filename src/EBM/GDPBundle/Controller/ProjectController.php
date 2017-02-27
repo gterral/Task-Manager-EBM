7480f63aa495e40aea43048e25f89608f53073d0
Guillaume Terral <gterral2@gmail.com>
2017-02-27 10:09:04 +0100

    Modification route Task/Project

--- src/EBM/GDPBundle/Controller/ProjectController.php
+++ src/EBM/GDPBundle/Controller/ProjectController.php
@@ -9,19 +9,6 @@ use EBM\UserInterfaceBundle\Entity\Project;
 
 class ProjectController extends Controller
 {
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @ParamConverter("project",options={"mapping": {"code":"code"}})
-     */
-    public function viewTasksAction(Project $project)
-    {
-        // On return la vue avec la liste des tâches
-        return $this->render('EBMGDPBundle:Project:index.html.twig',
-            array('listTasks' => $project->getTasks(),
-                'project' => $project
-            )
-        );
-    }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
