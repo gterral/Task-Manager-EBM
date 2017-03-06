164ee565dea38548db6130dc7dc4dbad348a7e33
Margot Quettelart <Margot@Margots-MacBook-Air.local>
2017-03-06 13:49:29 +0100

    Gerer les publications et les utilisateurs

--- src/EBM/SocialNetworkBundle/Controller/PublicationController.php
+++ src/EBM/SocialNetworkBundle/Controller/PublicationController.php
@@ -4,10 +4,26 @@ namespace EBM\SocialNetworkBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
+#Recuperer les tags que l'user like
+#Recuperer le projet de user
+#Recuperer les follows du user
+#Afficher tous les publications qui correspondent a l'un des trois
 class PublicationController extends Controller
 {
-    public function indexAction()
+    public function indexAction(User $user)
     {
-        return $this->render('EBMSocialNetworkBundle:Default:index.html.twig');
+        #FAUX
+        return $this->render('EBMSocialNetworkBundle:Publication:index.html.twig', array('listPubli' => $user->getTags(),
+            'project' => $project);
+
     }
 }
+
+#Vue d'une publication en détails lorsque je clique dessus
+
+#Ajout d'une publication
+
+#Supprimer une publication
+
+#Modifier une publication
