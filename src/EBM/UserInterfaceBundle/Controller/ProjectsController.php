b8fe062dff27ce427c2d6d2eb9a8e6b41c339105
Julien Atlan <julien.atlan@centrale.ec-lille.fr>
2017-03-21 23:03:00 +0100

    Ajout du formulaire de création de projet (front only)

--- src/EBM/UserInterfaceBundle/Controller/ProjectsController.php
+++ src/EBM/UserInterfaceBundle/Controller/ProjectsController.php
@@ -1,31 +1,27 @@
 <?php
 
-namespace Core\UserBundle\Controller;
+namespace EBM\UserInterfaceBundle\Controller;
 
-use Core\UserBundle\Entity\User;
+use EBM\UserInterfaceBundle\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
-
-class ProfileController extends Controller
+class ProjectsController extends Controller
 {
-    public function addUserAction(Request $request)
+    public function addProjectAction(Request $request)
     {
-        $user = new User();
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $user);
+        $project = new Project();
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $project);
 
         $formBuilder
-
             ->add('name', TextType::class)
-            ->add('surname', TextType::class)
-            ->add('email', TextType::class)
-            ->add('promotion', IntegerType::class)
-            ->add('desc', TextareaType::class)
+            ->add('projectType', TextType::class)
+            ->add('code', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('save', SubmitType::class)
         ;
 
@@ -40,27 +36,29 @@ class ProfileController extends Controller
             {
                 /* On rajoute les attributs manquants */
 
+                /*
                 $user->setUsername(strtolower(substr($user->getName(), 0, 1) . $user->getSurname())); // "Julien Atlan" donnera "jatlan"
                 $user->setPassword("user_ebm");
                 $user->setFullname($user->getName() . ' ' . $user->getSurname());
                 $user->addRole("ROLE_USER");
-
-                /* [À ÉVITER] On persiste l'objet
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($user);
-                $em->flush();
                 */
+
 
                 /* [ON UTILISERA CETTE MÉTHODE QUAND J'ARRIVERAI À FAIRE MARCHER DJANGO] */
                 /* On utilise l'API Django */
 
 
-                return $this->redirectToRoute('core_user_homepage');
+                return $this->redirectToRoute('ebm_user_project_creation');
             }
         }
 
-        return $this->render('CoreUserBundle:Profile:register.html.twig', array(
+        return $this->render('EBMUserInterfaceBundle:Projects:createProject.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    public function viewProjectAction($id)
+    {
+
     }
 }
